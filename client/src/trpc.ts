beb35bb2f5f2a6ff239766cad7acb858e07af8de
Nicolas Guerin <disconico@outlook.com>
2023-12-30 19:55:58 +0100

    log uri to test

--- client/src/trpc.ts
+++ client/src/trpc.ts
@@ -4,7 +4,10 @@ import type { AppRouter } from "../../server/src/index";
 
 export const api = createTRPCReact<AppRouter>();
 
-const url = process.env.NODE_ENV === "production" ? (process.env.SERVER_URI as string) : "http://localhost:3000/trpc";
+const url = process.env.NODE_ENV === "production" ? process.env.SERVER_URI! : "http://localhost:3000/trpc";
+
+console.log("NICO URL", url)
+
 
 export const trpcClient = api.createClient({
   links: [
