e74ebea8df3922d615e28c54285cd8baf66daf49
Nicolas Guerin <disconico@outlook.com>
2023-12-30 15:40:13 +0100

    add client dockerfile

--- client/src/trpc.ts
+++ client/src/trpc.ts
@@ -4,10 +4,15 @@ import type { AppRouter } from "../../server/src/index";
 
 export const api = createTRPCReact<AppRouter>();
 
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://https://server-elysia-api.fly.dev/trpc"
+    : "http://localhost:8080/trpc";
+
 export const trpcClient = api.createClient({
   links: [
     httpBatchLink({
-      url: "http://localhost:8080/trpc",
+      url,
     }),
   ],
 });
