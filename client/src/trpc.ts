bc2b7bc2298da4831fd74b7bedbeea21f25540fb
Nicolas Guerin <disconico@outlook.com>
2023-12-30 19:50:51 +0100

    add env variable for server uri

--- client/src/trpc.ts
+++ client/src/trpc.ts
@@ -4,10 +4,7 @@ import type { AppRouter } from "../../server/src/index";
 
 export const api = createTRPCReact<AppRouter>();
 
-const url =
-  process.env.NODE_ENV === "production"
-    ? "https://server-elysia-api.fly.dev/trpc"
-    : "http://localhost:3000/trpc";
+const url = process.env.NODE_ENV === "production" ? (process.env.SERVER_URI as string) : "http://localhost:3000/trpc";
 
 export const trpcClient = api.createClient({
   links: [
