ad9c4ed45f6f08b21a40bc0c5a4013f2a5125d4e
Nicolas Guerin <disconico@outlook.com>
2024-01-02 19:23:53 +0100

    add TRPCError

--- server/src/post/post.router.ts
+++ server/src/post/post.router.ts
@@ -1,25 +1,32 @@
 import { router, publicProcedure } from "../trpc";
-import { insertPostSchema, posts, selectPostSchema } from "../db/schema";
+import { insertPostSchema, posts } from "../db/schema";
+import { TRPCError } from "@trpc/server";
 
 export const postRouter = router({
   hello: publicProcedure.query(() => {
     return "hello";
   }),
 
-  createPost: publicProcedure
-    .input(insertPostSchema)
-    .mutation(async ({ ctx, input }) => {
-      const post = await ctx.db.insert(posts).values(insertPostSchema.parse(input))
+  createPost: publicProcedure.input(insertPostSchema).mutation(async ({ ctx, input }) => {
+    try {
+      const post = await ctx.db.insert(posts).values(insertPostSchema.parse(input));
       return post;
-    }),
+    } catch (err) {
+      console.error(err);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to create post.",
+      });
+    }
+  }),
 
   getFirstPost: publicProcedure.query(async ({ ctx }) => {
     const post = await ctx.db.query.posts.findFirst({});
-    return selectPostSchema.parse(post);
+    return post;
   }),
 
   getPosts: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.db.query.posts.findMany();
-     return posts.map((post) => selectPostSchema.parse(post));
+    return posts;
   }),
 });
